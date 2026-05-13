84950ca6ec8c6a7da3f18ec30c056cd9c4641cf0
Gary (Chau) Nguyen <Chau.Nguyen@enlyte.com>
2026-05-12 19:02:07 -0700

    test

---
+++ run.bat
@@ -0,0 +1,3 @@
+az login
+
+"C:/Users/cn101028r/AppData/Local/Programs/Python/Python311/python.exe" scripts/bulk_create_children.py --project "Terra Software Development" --query-id bb06f762-a087-48e4-899f-daf7ff9f0411
